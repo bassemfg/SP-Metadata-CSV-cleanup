16fb617061c4b99402e8b169987fda6faab0caa5
Bassem Yacoube <bassemfg@gmx.net>
2018-06-26 23:10:42 -0700

    3

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -13,6 +13,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+
             int i = 0;
             DataTable dt = null;
             DataTable dtMerged = new DataTable("Metadata");
@@ -36,26 +37,28 @@ namespace ConsoleApp2
                 {
                     //read a line in the CSV file
                     line = sr.ReadLine();
-                    // if it is not a header, i.e not start of a new table, add a new row to target
-                    if (!line.StartsWith("SourcePath"))
-                        dt.Rows.Add(dt.NewRow());
-                    else //if it is a header i.e. start of new table
-                    {
-                        // merge with existing table, if it exists
-                        if (dt != null)
-                            dtMerged.Merge(dt, true, MissingSchemaAction.Add);
-                        ht = new Hashtable();
-                        dt = new DataTable();
-                    }
                     if (line.Trim().Length > 0)
                     {
+                        // if it is not a header, i.e not start of a new table, add a new row to target
+                        if (!line.StartsWith("SourcePath"))
+                            dt.Rows.Add(dt.NewRow());
+                        else //if it is a header i.e. start of new table
+                        {
+                            // merge with existing table, if it exists
+                            if (dt != null)
+                                dtMerged.Merge(dt, true, MissingSchemaAction.Add);
+                            ht = new Hashtable();
+                            dt = new DataTable();
+
+                        }
+
 
                         sbFileRemoveSpaces.Append(line);
                         sbFileRemoveSpaces.Append(@"
 ");
                         columns = line.Split(',');
                         i = 0;
-                        
+
                         foreach (string s in columns)
                         {
 
@@ -64,22 +67,22 @@ namespace ConsoleApp2
                                 if (dt.Columns.Contains(s) == false)
                                 {
                                     dt.Columns.Add(s);
-                                    ht.Add( i,s);
+                                    ht.Add(i, s);
                                 }
                                 else // to solve the problem of columns with same name
                                 {
                                     if (dt.Columns.Contains(s + i.ToString()) == false)
                                     {
                                         dt.Columns.Add(s + i.ToString());
-                                        ht.Add( i, s + i.ToString());
+                                        ht.Add(i, s + i.ToString());
                                     }
                                 }
 
 
-                                if (headers.Contains(s)==false)
+                                if (headers.Contains(s) == false)
                                 {
                                     headers.Enqueue(s);
-                                
+
                                     sb.Append(s);
                                     sb.Append(@"
 ");
@@ -88,7 +91,7 @@ namespace ConsoleApp2
 
                             else
                             {
-                                if (i < dt.Columns.Count )
+                                if (i < dt.Columns.Count)
                                     dt.Rows[dt.Rows.Count - 1][ht[i].ToString()] = s;//dt.Columns[i].ColumnName
                             }
 
@@ -110,14 +113,15 @@ namespace ConsoleApp2
             sw.Write(sb.ToString());
             sw.Flush();
             sw.Close();
-/*
-            sw = new StreamWriter(@"c:\test\allmetadata.csv");
-            sw.Write(sbFileRemoveSpaces.ToString());
-            sw.Flush();
-            sw.Close();
-            */
+            /*
+                        sw = new StreamWriter(@"c:\test\allmetadata.csv");
+                        sw.Write(sbFileRemoveSpaces.ToString());
+                        sw.Flush();
+                        sw.Close();
+                        */
             dtMerged.WriteXml(@"c:\test\allmetadata.xml");
-
         }
+
     }
 }
+
